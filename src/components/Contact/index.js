9bd0c2ef3393601432c63e27b357cd7ef34a6bf5
bhavani-shankar <bhavani.shankar037@gmail.com>
2023-12-31 16:07:33 +0530

    completed the potfolio website

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_yaaykwh', 'template_gym4rsb', form.current, 'BGq6ISr12rOe8mm2Z')
       .then((result) => {
         setOpen(true);
         form.current.reset();
